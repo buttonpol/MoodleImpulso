954319c2a1b92e377e4180b66de899b57461fd43
pol <buttonpol@gmail.com>
2016-08-13 12:35:12 -0300

    corregido menu y agregado reporte

---
+++ moodle/report/reporteimpulso/index.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Created by PhpStorm.
+ * User: pol
+ * Date: 13/8/2016
+ * Time: 12:11
+ *
+ * @copyright  2016 - A+ ABC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+?>
+<!DOCTYPE html>
+<html ng-app="AppPrueba">
+<head>
+    <meta charset="UTF-8">
+    <title>Gráfica barras</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+    <script src="app.js"></script>
+</head>
+
+<body onload="cargarDatos()">
+
+</body>
+
+</html>
